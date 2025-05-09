b169c5bcdbb6656fd839886443893b01f91d7ec5
thatsmare <mpenkowska0@gmail.com>
2025-05-09 19:48:13 +0200

    Nominator and denominator mistake fix

--- main.py
+++ main.py
@@ -60,6 +60,7 @@ class Window(QMainWindow):
         self.setCentralWidget(widget)
 
         #WARTOŚCI POCZĄTKOWE ALE FINALNIE NA PEWNO NIE W TYM MIEJSCU
+        self.b4 = 0
         self.b3 = 0
         self.b2 = 0
         self.b1 = 0
@@ -72,15 +73,15 @@ class Window(QMainWindow):
 
     def menu_display(self):
         menu_view = QVBoxLayout()
-        self.nominator_b3_input = QLineEdit(str(self.b3))
-        self.nominator_b2_input = QLineEdit(str(self.b2))
-        self.nominator_b1_input = QLineEdit(str(self.b1))
-        self.nominator_b0_input = QLineEdit(str(self.b0))
-        self.denominator_a4_input = QLineEdit(str(self.a4))
-        self.denominator_a3_input = QLineEdit(str(self.a3))
-        self.denominator_a2_input = QLineEdit(str(self.a2))
-        self.denominator_a1_input = QLineEdit(str(self.a1))
-        self.denominator_a0_input = QLineEdit(str(self.a0))
+        self.nominator_a3_input = QLineEdit(str(self.a3))
+        self.nominator_a2_input = QLineEdit(str(self.a2))
+        self.nominator_a1_input = QLineEdit(str(self.a1))
+        self.nominator_a0_input = QLineEdit(str(self.a0))
+        self.denominator_b4_input = QLineEdit(str(self.b4))
+        self.denominator_b3_input = QLineEdit(str(self.b3))
+        self.denominator_b2_input = QLineEdit(str(self.b2))
+        self.denominator_b1_input = QLineEdit(str(self.b1))
+        self.denominator_b0_input = QLineEdit(str(self.b0))
         set_b = QPushButton("Simulate")
         back_b = QPushButton("Back to start")
 
@@ -89,87 +90,87 @@ class Window(QMainWindow):
 
         menu_view.addWidget(QLabel("<h3>Coefficients of transfer function</h3>"))
         menu_view.addWidget(QLabel("Nominator of G :"))
-        self.nominator_b3_input.setFixedWidth(80)
-        self.nominator_b3_input.editingFinished.connect(lambda: self.update_coefficient(self.nominator_b3_input, "b3"))
-        b3_layout = QHBoxLayout()
-        b3_layout.setAlignment(Qt.AlignLeft)
-        b3_layout.addWidget(QLabel("b3:"))
-        b3_layout.addWidget(self.nominator_b3_input)
-        b3_layout.addStretch()
-        menu_view.addLayout(b3_layout)
-
-        self.nominator_b2_input.setFixedWidth(80)
-        self.nominator_b2_input.editingFinished.connect(lambda: self.update_coefficient(self.nominator_b2_input, "b2"))
-        b2_layout = QHBoxLayout()
-        b2_layout.setAlignment(Qt.AlignLeft)
-        b2_layout.addWidget(QLabel("b2:"))
-        b2_layout.addWidget(self.nominator_b2_input)
-        b2_layout.addStretch()
-        menu_view.addLayout(b2_layout)
-
-        self.nominator_b1_input.setFixedWidth(80)
-        self.nominator_b1_input.editingFinished.connect(lambda: self.update_coefficient(self.nominator_b1_input, "b1"))
-        b1_layout = QHBoxLayout()
-        b1_layout.setAlignment(Qt.AlignLeft)
-        b1_layout.addWidget(QLabel("b1:"))
-        b1_layout.addWidget(self.nominator_b1_input)
-        b1_layout.addStretch()
-        menu_view.addLayout(b1_layout)
-
-        self.nominator_b0_input.setFixedWidth(80)
-        self.nominator_b0_input.editingFinished.connect(lambda: self.update_coefficient(self.nominator_b0_input, "b0"))
-        b0_layout = QHBoxLayout()
-        b0_layout.setAlignment(Qt.AlignLeft)
-        b0_layout.addWidget(QLabel("b0:"))
-        b0_layout.addWidget(self.nominator_b0_input)
-        b0_layout.addStretch()
-        menu_view.addLayout(b0_layout)
-
-        menu_view.addWidget(QLabel("Denominator of G :"))
-        self.denominator_a4_input.setFixedWidth(80)
-        self.denominator_a4_input.editingFinished.connect(lambda: self.update_coefficient(self.denominator_a4_input, "a4"))
-        a4_layout = QHBoxLayout()
-        a4_layout.setAlignment(Qt.AlignLeft)
-        a4_layout.addWidget(QLabel("a4:"))
-        a4_layout.addWidget(self.denominator_a4_input)
-        a4_layout.addStretch()
-        menu_view.addLayout(a4_layout)
-
-        self.denominator_a3_input.setFixedWidth(80)
-        self.denominator_a3_input.editingFinished.connect(lambda: self.update_coefficient(self.denominator_a3_input, "a3"))
+        self.nominator_a3_input.setFixedWidth(80)
+        self.nominator_a3_input.editingFinished.connect(lambda: self.update_coefficient(self.nominator_a3_input, "a3"))
         a3_layout = QHBoxLayout()
         a3_layout.setAlignment(Qt.AlignLeft)
         a3_layout.addWidget(QLabel("a3:"))
-        a3_layout.addWidget(self.denominator_a3_input)
+        a3_layout.addWidget(self.nominator_a3_input)
         a3_layout.addStretch()
         menu_view.addLayout(a3_layout)
 
-        self.denominator_a2_input.setFixedWidth(80)
-        self.denominator_a2_input.editingFinished.connect(lambda: self.update_coefficient(self.denominator_a2_input, "a2"))
+        self.nominator_a2_input.setFixedWidth(80)
+        self.nominator_a2_input.editingFinished.connect(lambda: self.update_coefficient(self.nominator_a2_input, "a2"))
         a2_layout = QHBoxLayout()
         a2_layout.setAlignment(Qt.AlignLeft)
         a2_layout.addWidget(QLabel("a2:"))
-        a2_layout.addWidget(self.denominator_a2_input)
+        a2_layout.addWidget(self.nominator_a2_input)
         a2_layout.addStretch()
         menu_view.addLayout(a2_layout)
 
-        self.denominator_a1_input.setFixedWidth(80)
-        self.denominator_a1_input.editingFinished.connect(lambda: self.update_coefficient(self.denominator_a1_input, "a1"))
+        self.nominator_a1_input.setFixedWidth(80)
+        self.nominator_a1_input.editingFinished.connect(lambda: self.update_coefficient(self.nominator_a1_input, "a1"))
         a1_layout = QHBoxLayout()
         a1_layout.setAlignment(Qt.AlignLeft)
         a1_layout.addWidget(QLabel("a1:"))
-        a1_layout.addWidget(self.denominator_a1_input)
+        a1_layout.addWidget(self.nominator_a1_input)
         a1_layout.addStretch()
         menu_view.addLayout(a1_layout)
 
-        self.denominator_a0_input.setFixedWidth(80)
-        self.denominator_a0_input.editingFinished.connect(lambda: self.update_coefficient(self.denominator_a0_input, "a0"))
+        self.nominator_a0_input.setFixedWidth(80)
+        self.nominator_a0_input.editingFinished.connect(lambda: self.update_coefficient(self.nominator_a0_input, "a0"))
         a0_layout = QHBoxLayout()
         a0_layout.setAlignment(Qt.AlignLeft)
         a0_layout.addWidget(QLabel("a0:"))
-        a0_layout.addWidget(self.denominator_a0_input)
+        a0_layout.addWidget(self.nominator_a0_input)
         a0_layout.addStretch()
         menu_view.addLayout(a0_layout)
+
+        menu_view.addWidget(QLabel("Denominator of G :"))
+        self.denominator_b4_input.setFixedWidth(80)
+        self.denominator_b4_input.editingFinished.connect(lambda: self.update_coefficient(self.denominator_b4_input, "b4"))
+        b4_layout = QHBoxLayout()
+        b4_layout.setAlignment(Qt.AlignLeft)
+        b4_layout.addWidget(QLabel("b4:"))
+        b4_layout.addWidget(self.denominator_b4_input)
+        b4_layout.addStretch()
+        menu_view.addLayout(b4_layout)
+
+        self.denominator_b3_input.setFixedWidth(80)
+        self.denominator_b3_input.editingFinished.connect(lambda: self.update_coefficient(self.denominator_b3_input, "b3"))
+        b3_layout = QHBoxLayout()
+        b3_layout.setAlignment(Qt.AlignLeft)
+        b3_layout.addWidget(QLabel("b3:"))
+        b3_layout.addWidget(self.denominator_b3_input)
+        b3_layout.addStretch()
+        menu_view.addLayout(b3_layout)
+
+        self.denominator_b2_input.setFixedWidth(80)
+        self.denominator_b2_input.editingFinished.connect(lambda: self.update_coefficient(self.denominator_b2_input, "b2"))
+        b2_layout = QHBoxLayout()
+        b2_layout.setAlignment(Qt.AlignLeft)
+        b2_layout.addWidget(QLabel("b2:"))
+        b2_layout.addWidget(self.denominator_b2_input)
+        b2_layout.addStretch()
+        menu_view.addLayout(b2_layout)
+
+        self.denominator_b1_input.setFixedWidth(80)
+        self.denominator_b1_input.editingFinished.connect(lambda: self.update_coefficient(self.denominator_b1_input, "b1"))
+        b1_layout = QHBoxLayout()
+        b1_layout.setAlignment(Qt.AlignLeft)
+        b1_layout.addWidget(QLabel("b1:"))
+        b1_layout.addWidget(self.denominator_b1_input)
+        b1_layout.addStretch()
+        menu_view.addLayout(b1_layout)
+
+        self.denominator_b0_input.setFixedWidth(80)
+        self.denominator_b0_input.editingFinished.connect(lambda: self.update_coefficient(self.denominator_b0_input, "b0"))
+        b0_layout = QHBoxLayout()
+        b0_layout.setAlignment(Qt.AlignLeft)
+        b0_layout.addWidget(QLabel("b0:"))
+        b0_layout.addWidget(self.denominator_b0_input)
+        b0_layout.addStretch()
+        menu_view.addLayout(b0_layout)
 
         menu_view.addWidget(QLabel("<h3>Input signal</h3>"))
         signal_group_box = QGroupBox("Wybór sygnału pobudzającego")
@@ -196,7 +197,14 @@ class Window(QMainWindow):
         menu_widget.setLayout(menu_view)
         self.setCentralWidget(menu_widget)
 
+    # tf coefficiants for easy access 
+    def get_tf_coefficients(self):
+        nominator = [self.a4, self.a3, self.a2, self.a1, self.a0] #licznikGÓRA
+        denominator = [self.b3, self.b2, self.b1, self.b0] #mianownikDÓŁ
+        return nominator, denominator
+
     def simulation(self):
+
         simulation_view= QVBoxLayout()
 
         title = QLabel("<h3>Symulacja układu</h3>")
@@ -212,6 +220,10 @@ class Window(QMainWindow):
         simulation_widget.setLayout(simulation_view)
         self.setCentralWidget(simulation_widget)
 
+        # Bode plot part
+
+    
+    
 #run
 if __name__ == "__main__":
     app = QApplication(sys.argv)
@@ -257,3 +269,7 @@ class bode:
     def system_stability():
 
 """
+"""class BodePlot:
+    def __init__(self, nominator, denominator):
+"""      
+        
